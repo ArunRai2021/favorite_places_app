9ce0125482c412efecba0c07c9e550fe300b15b9
ArunRai2021 <arunraihrit@gmail.com>
2025-08-01 14:37:09 +0530

    changes done for image picker or done

---
+++ lib/widgets/places_list.dart
@@ -0,0 +1,47 @@
+import 'package:favorite_places_app/model/place.dart';
+import 'package:favorite_places_app/screen/places_detail.dart';
+import 'package:flutter/material.dart';
+
+class PlacesList extends StatelessWidget {
+  const PlacesList({super.key, required this.places});
+
+  final List<Place> places;
+
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return const Center(
+        child: Text(
+          "No places added Yet",
+          style: TextStyle(
+            fontSize: 12,
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      );
+    }
+    return ListView.builder(
+      itemCount: places.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text(
+            places[index].title,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          ),
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return PlaceDetailScreen(place: places[index]);
+                },
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
