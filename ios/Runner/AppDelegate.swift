5e5b78a532b856ac87e366c6609b850f1f512c7b
ArunRai2021 <arunraihrit@gmail.com>
2025-08-06 13:37:44 +0530

    add google Map screen are done

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,14 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyB7m43S0IDA5BKIaTdvYW9l7htSZ-0Hu5I)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/screen/places_detail.dart
+++ lib/screen/places_detail.dart
@@ -1,7 +1,6 @@
 import 'package:favorite_places_app/model/place.dart';
+import 'package:favorite_places_app/screen/map.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
 
 class PlaceDetailScreen extends StatelessWidget {
   const PlaceDetailScreen({super.key, required this.place});
@@ -39,46 +38,22 @@ class PlaceDetailScreen extends StatelessWidget {
               right: 0,
               child: Column(
                 children: [
-                  CircleAvatar(
-                    radius: 70,
-                    backgroundColor: Colors.grey[200],
-                    // Optional: background color
-                    child: ClipOval(
-                      child: SizedBox(
-                        width: 140, // 2 * radius
-                        height: 140,
-                        child: FlutterMap(
-                          options: MapOptions(
-                            initialCenter: LatLng(
-                              place.location.latitude,
-                              place.location.longitude,
-                            ),
-                            initialZoom: 16,
-                          ),
-                          children: [
-                            TileLayer(
-                              urlTemplate:
-                                  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                              subdomains: ['a', 'b', 'c'],
-                            ),
-                            MarkerLayer(
-                              markers: [
-                                Marker(
-                                  point: LatLng(
-                                    place.location.latitude,
-                                    place.location.longitude,
-                                  ),
-                                  child: const Icon(
-                                    Icons.location_on,
-                                    size: 40,
-                                    color: Colors.red,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return MapScreen(
+                              location: place.location,
+                              isSelecting: false,
+                            );
+                          },
                         ),
-                      ),
+                      );
+                    },
+                    child: CircleAvatar(
+                      radius: 70,
+                      backgroundImage: NetworkImage(locationImage),
                     ),
                   ),
                   Container(
